f9c3ce50f9eeb1253a8c4f7c9ed32edd9e84f53a
Enrico Meloni <10127473+ZenPluS@users.noreply.github.com>
2025-10-31 21:59:33 +0100

    Rework as shared projects

--- DevEn.Xrm.Abstraction.Plugins.UnitTests/Properties/AssemblyInfo.cs
+++ DevEn.Xrm.Abstraction.Plugins.UnitTests/Properties/AssemblyInfo.cs
@@ -1,19 +1,24 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting; // Added for parallelization attribute
 
 [assembly: AssemblyTitle("DevEn.Xrm.Abstraction.Plugins.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DevEn.Xrm.Abstraction.Plugins.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright ©2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("e72139d5-956a-4d70-a2e9-c985c24af8f7")]
+[assembly: Guid("569d4747-a1da-4e2d-b9e4-072326cabba3")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Explicitly disable test parallelization to satisfy MSTEST0001 (change to Parallelize if desired)
+[assembly: DoNotParallelize]
--- DevEn.Xrm.Abstraction.Workflows.UnitTests/Properties/AssemblyInfo.cs
+++ DevEn.Xrm.Abstraction.Workflows.UnitTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,24 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting; // Added for parallelization attribute
 
 [assembly: AssemblyTitle("DevEn.Xrm.Abstraction.Workflows.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DevEn.Xrm.Abstraction.Workflows.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright ©2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("9fa9645a-63b4-498a-8c11-467d38253f0d")]
+[assembly: Guid("5181cb4d-67c7-4b2a-b705-8872fef4ee7f")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Explicitly disable test parallelization to satisfy MSTEST0001 (change to Parallelize if desired)
+[assembly: DoNotParallelize]
